a0baf1b040eb01f656053651deb1bd53fb09e2e7
varunmaharana <varunmaharana14@gmail.com>
2023-11-03 22:22:12 +0530

    login route

--- server/routes.js
+++ server/routes.js
@@ -1,13 +1,17 @@
 import express from "express";
 import User from "./models/User.js"
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
+
+const secret = "josdbfasd8f23hr923br32fi3d9d32bu8934rf";
+
 
 router.post("/register", async (req, res) => {
     const { username, password } = req.body;
     try {
         // Creating new user entry
-        const userInfo = User.create({
+        const userInfo = await User.create({
             username,
             password,
         });
@@ -20,6 +24,36 @@ router.post("/register", async (req, res) => {
             success: false, 
             err,
         });
+    }
+});
+
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const fetchedUserInfo = await User.findOne({ username });
+    if (fetchedUserInfo.password === password) {
+        jwt.sign(
+            {
+                username,
+                id: fetchedUserInfo._id
+            },
+            secret,
+            {},
+            (err, token) => {
+                if (err) {
+                    throw err;
+                }
+                res.cookie("contact_app_login_token", token).json({
+                    success: true,
+                    username,
+                    id: fetchedUserInfo._id,
+                });
+            }
+        );
+    } else {
+        res.status(400).json({
+            success: false,
+            message: "Invalid credentials!"
+        })
     }
 });
 
